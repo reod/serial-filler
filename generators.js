e582559d77bf50b23ce4d2aa9f48db95c3ba6f01
Remek Ambroziak <remek.ambroziak@gmail.com>
2016-09-10 17:22:17 +0200

    Implement lorem ipsum generation

---
+++ generators.js
@@ -0,0 +1,83 @@
+function generatePESEL(sex) {
+  const weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+  const date = getRandomDate();
+  const fullYear = date.getFullYear();
+  const y = fullYear % 100;
+  let m = date.getMonth()+1;
+  const d = date.getDate();
+    
+  if (fullYear >= 1800 && fullYear <= 1899) {
+    m += 80;
+  } else if (fullYear >= 2000 && fullYear <= 2099) {
+    m += 20;
+  } else if (fullYear >= 2100 && fullYear <= 2199) {
+    m += 40;
+  } else if (fullYear >= 2200 && fullYear <= 2299) {
+    m += 60;
+  }
+    
+  let numbers = [Math.floor(y/10), y%10, Math.floor(m/10), m%10, Math.floor(d/10), d%10];
+
+  for (let i = numbers.length; i < weights.length - 1; i++) {
+    numbers[i] = getRandomDigit();
+  }
+
+  if (sex === 'M') {
+    numbers[weights.length - 1] = getRandomDigitFrom('13579');
+  } else if (sex === 'F') {
+    numbers[weights.length - 1] = getRandomDigitFrom('02468');
+  } else {
+    numbers[weights.length - 1] = getRandomDigit();
+  }
+      
+  let controlNumber = 0;
+  
+  for (let i = 0; i < numbers.length; i++) {
+    controlNumber += weights[i] * numbers[i];
+  }
+  
+  controlNumber = (10 - (controlNumber % 10)) % 10;
+              
+  let pesel = '';
+  for (let i = 0; i < numbers.length; i++) {
+    pesel += numbers[i];
+  }
+
+  pesel += controlNumber;  
+  
+  return pesel; 
+};
+
+function getRandomDate() {
+  const from = new Date(1800, 0, 1).getTime();
+  const to = new Date(2299, 0, 1).getTime();
+
+  return new Date(from + Math.random() * (to - from));
+};
+
+function getRandomDigit() {
+  return Math.floor(Math.random() * 10);
+};
+  
+function getRandomDigitFrom(digits = '0123456789') {
+  const i = Math.floor(Math.random() * digits.length);
+  return digits[i];
+};
+
+function generateLoremIpsum() {
+  return (
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend, leo eu eleifend ullamcorper, elit arcu mattis lectus, ut bibendum nunc turpis sit amet ante. Aenean et purus et enim ultricies tincidunt sit amet et urna. Maecenas vestibulum at urna a porttitor. Duis commodo diam feugiat nunc pulvinar, sed efficitur leo blandit. Etiam dignissim elementum lectus vitae elementum. Nam lacinia varius elementum. Quisque consectetur quam eget molestie ornare. Proin vestibulum, orci eget iaculis consectetur, risus eros dictum nunc, et sodales tellus quam vel nibh. Proin luctus rutrum consectetur. Etiam et justo sit amet nisi eleifend fermentum. Vivamus nibh quam, pharetra non purus id, consequat rhoncus urna. Nulla facilisi. Vivamus tincidunt condimentum sapien, at aliquam mauris rhoncus eget. Quisque pretium bibendum rhoncus.
+    Aenean molestie fringilla sapien, sit amet feugiat ligula elementum ultricies. Phasellus eget odio eget elit lacinia pulvinar fermentum eu libero. Donec luctus mauris sed tortor suscipit, in bibendum metus bibendum. In quis nibh nec mi sagittis lobortis. Praesent id rutrum ante. Nunc bibendum ipsum eu dui placerat, id vestibulum felis elementum. Mauris ac ultrices leo, quis aliquam elit. In in tellus non enim pretium mollis. Phasellus faucibus tempus mollis. Nunc in augue id neque malesuada hendrerit at et urna. Sed ac efficitur augue. Phasellus mi nisl, interdum in tincidunt sit amet, dignissim vel ex. Maecenas ut tellus mattis, posuere dolor ac, elementum nisi. Duis aliquet ligula eu dui blandit, in efficitur tellus maximus.
+    Morbi quis metus pretium, gravida sem rutrum, bibendum augue. Aliquam tincidunt commodo magna sed porttitor. Donec in turpis imperdiet, tempor neque a, luctus metus. Maecenas id nisi massa. Curabitur porttitor nibh mauris. Aenean eget arcu semper, convallis nunc ut, auctor magna. Sed feugiat nibh et justo ultricies feugiat.
+    Nam cursus tristique diam vitae faucibus. Nullam faucibus ante a quam consectetur, et condimentum elit faucibus. Quisque fermentum pellentesque venenatis. Aliquam molestie ligula pellentesque, maximus purus ac, egestas nulla. Aenean semper efficitur enim, eu fermentum mi. Sed egestas ornare felis, ac tempor elit dapibus eu. Phasellus fringilla eget risus in feugiat. Suspendisse nunc neque, auctor non sodales at, vehicula sed lectus. Ut sed nunc sit amet velit eleifend elementum sit amet non turpis.
+    Pellentesque vehicula erat vitae augue aliquam, eget dictum ipsum convallis. Sed id convallis ipsum. Etiam urna ligula, eleifend sit amet ante vel, laoreet ullamcorper ante. Donec non tortor dictum, sagittis sem et, dapibus elit. In condimentum velit nunc, nec blandit magna sodales at. Sed venenatis, lectus sit amet euismod facilisis, sem mauris maximus metus, et fermentum orci velit a diam. Integer a arcu velit. Aliquam vitae tortor quis libero congue commodo.`
+  );
+};
+
+function generateLoremIpsumParagraph() {
+  return generateLoremIpsum().split('\n')[0];
+};
+
+function generateLoremIpsumSentence() {
+  return generateLoremIpsum().split('.')[0];
+};
--- serial-filler.js
+++ serial-filler.js
@@ -11,7 +11,22 @@ const serialFiller = {
     {
       menuText: 'PESEL kobiety',
       generatorFn: generatePESEL.bind('F')
-    }
+    },
+
+    {
+      menuText: 'Jedno zdanie',
+      generatorFn: generateLoremIpsum
+    },
+
+    {
+      menuText: 'Cały akapit',
+      generatorFn: generateLoremIpsumParagraph
+    },
+
+    {
+      menuText: 'Kilka akapitów',
+      generatorFn: generateLoremIpsum
+    },
   ],
   
   init() {
@@ -37,71 +52,5 @@ const serialFiller = {
   }
 };
 
+
 serialFiller.init();
-
-
-function generatePESEL(sex) {
-  const weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
-  const date = getRandomDate();
-  const fullYear = date.getFullYear();
-  const y = fullYear % 100;
-  let m = date.getMonth()+1;
-  const d = date.getDate();
-    
-  if (fullYear >= 1800 && fullYear <= 1899) {
-    m += 80;
-  } else if (fullYear >= 2000 && fullYear <= 2099) {
-    m += 20;
-  } else if (fullYear >= 2100 && fullYear <= 2199) {
-    m += 40;
-  } else if (fullYear >= 2200 && fullYear <= 2299) {
-    m += 60;
-  }
-    
-  let numbers = [Math.floor(y/10), y%10, Math.floor(m/10), m%10, Math.floor(d/10), d%10];
-
-  for (let i = numbers.length; i < weights.length - 1; i++) {
-    numbers[i] = getRandomDigit();
-  }
-
-  if (sex === 'M') {
-    numbers[weights.length - 1] = getRandomDigitFrom('13579');
-  } else if (sex === 'F') {
-    numbers[weights.length - 1] = getRandomDigitFrom('02468');
-  } else {
-    numbers[weights.length - 1] = getRandomDigit();
-  }
-      
-  let controlNumber = 0;
-  
-  for (let i = 0; i < numbers.length; i++) {
-    controlNumber += weights[i] * numbers[i];
-  }
-  
-  controlNumber = (10 - (controlNumber % 10)) % 10;
-              
-  let pesel = '';
-  for (let i = 0; i < numbers.length; i++) {
-    pesel += numbers[i];
-  }
-
-  pesel += controlNumber;  
-  
-  return pesel; 
-};
-
-function getRandomDate() {
-  const from = new Date(1800, 0, 1).getTime();
-  const to = new Date(2299, 0, 1).getTime();
-
-  return new Date(from + Math.random() * (to - from));
-};
-
-function getRandomDigit() {
-  return Math.floor(Math.random() * 10);
-};
-  
-function getRandomDigitFrom(digits = '0123456789') {
-  const i = Math.floor(Math.random() * digits.length);
-  return digits[i];
-};
