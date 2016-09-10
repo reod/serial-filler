63eb78dcf95bb2b8ba31e3bbda25b5a0af6d5ca8
Remek Ambroziak <remek.ambroziak@gmail.com>
2016-09-10 16:55:05 +0200

    Implement PESEL generation

--- serial-filler-receiver.js
+++ serial-filler-receiver.js
@@ -1,3 +1,24 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    alert('got' + request.generatedValue);
+let clickedEl = null;
+
+
+document.addEventListener('mousedown', e => {
+  clickedEl = null;
+
+  // test only right click
+  if (e.button !== 2) {
+    return;
+  }
+
+  let tagName = e.target.tagName.toLowerCase();
+  if (tagName === 'textarea' || tagName === 'input') {
+    clickedEl = e.target;
+  }
+});
+
+chrome.runtime.onMessage.addListener(request => {
+  if (!clickedEl || !request || !request.value) {
+    return;
+  }
+
+  clickedEl.value = request.value;
 });
